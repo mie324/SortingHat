fdc759da8489f5979918662b0bfc3d8c8e1ce3f8
markohuang <marko.huang@mail.utoronto.ca>
2018-11-10 14:33:12 -0500

    Google Image Scraping Code

---
+++ image_scrapping.py
@@ -0,0 +1,17 @@
+from google_images_download import google_images_download
+
+# class instantiation
+response = google_images_download.googleimagesdownload()
+
+# creating list of arguments
+# list of used keywords
+# "paper coffee cups -art,juice box -art,snack packaging garbage"
+
+arguments = {"keywords": "take out box",
+             "limit": 500, "print_urls": True,
+             "chromedriver": "C:\\Users\\iatey\\PycharmProjects\\mie324\\SortingHat\\chromedriver.exe",
+             "output_directory": './SortingHat/images_Google'}
+# passing the arguments to the function
+paths = response.download(arguments)
+# printing absolute paths of the downloaded images
+print(paths)
