2a14aab39069f18539091a469e02e46f00de879e
markohuang <marko.huang@mail.utoronto.ca>
2018-11-25 15:22:39 -0500

    test prep: returns list of img tensors, labels, and description

--- test_data_prep.py
+++ test_data_prep.py
@@ -1,6 +1,7 @@
 from torchvision import transforms
 from PIL import Image
 import torch
+import re
 import glob
 import logging
 logging.basicConfig(
@@ -9,20 +10,26 @@ logging.basicConfig(
 
 transform = transforms.Compose([
                 transforms.RandomResizedCrop(128, (1, 1), (1, 1)),
-                transforms.Resize(299), # 299 for inception / 224 for resnet
+                transforms.Resize(299),  # 299 for Inception / 224 for ResNet
                 transforms.ToTensor(),
                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-img_list = []
-labels = []
-CLASSES = ['landfill', 'container', 'paper', 'coffeecups']
-for label in range(4):
-    fd = CLASSES[label]
-    logging.info('Currently at class {}'.format(fd))
-    for fmt in ['png']:
-        logging.info('-- at format {}'.format(fmt))
-        for filename in glob.glob('./data/{}/*.{}'.format(fd, fmt)):
+
+
+def test():
+    img_list = []
+    labels = []
+    descriptions = []
+    CLASSES = ['landfill', 'container', 'paper', 'coffeecups']
+    for label in range(4):
+        fd = CLASSES[label]
+        logging.info('Currently at class {}'.format(fd))
+        for fmt in ['jpg']:
+            for filename in glob.glob('./test/{}/*.{}'.format(fd, fmt)):
+                fname = re.sub(r'\./test/\w*\\', '', filename)
+                descriptions.append(re.sub(r'.JPG$', '', fname))
                 im = Image.open(filename)
                 im = transform(im)
                 labels.append(label)
                 img_list.append(im)
-img_tensor = torch.Tensor(img_list)
+    # img_tensor = torch.Tensor(img_list)
+    return img_list, labels, descriptions
